387e4d443c721760d476dbb0bd7a1291851a3303
Gyeongmin Lee <metrostile@gmail.com>
2018-11-18 21:09:51 -0800

    toggle animatio duration added

--- js/task-list.js
+++ js/task-list.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
-    $(".content--inprogress-people").on("click", function(e) {
-        $(this).parent().nextAll().eq(1).hide();
-        $(this).parent().nextAll().eq(0).toggle();
-        $(this).children().toggleClass("rotated");
-        $(this).next().children().removeClass("rotated");
-    });
+    toggleList();    
 
-    $(".content--done-people").on("click", function(e) {
-        $(this).parent().nextAll().eq(0).hide();
-        $(this).parent().nextAll().eq(1).toggle();
-        $(this).children().toggleClass("rotated");
-        $(this).prev().children().removeClass("rotated");
-    });
+    function toggleList() {
+        var toggleDuration = 100;
 
-    $(".close-button-img--people").on("click", function(e) {
-        $(this).parent().parent().hide();
-        $(this).parent().parent().siblings(".box-bottom").children().children().removeClass("rotated");
-    });
+        $(".content--inprogress-people").on("click", function(e) {
+            $(this).parent().nextAll().eq(1).hide();
+            $(this).parent().nextAll().eq(0).slideToggle(toggleDuration);
+            $(this).children().toggleClass("rotated");
+            $(this).next().children().removeClass("rotated");
+        });
+    
+        $(".content--done-people").on("click", function(e) {
+            $(this).parent().nextAll().eq(0).hide();
+            $(this).parent().nextAll().eq(1).slideToggle(toggleDuration);
+            $(this).children().toggleClass("rotated");
+            $(this).prev().children().removeClass("rotated");
+        });
+    
+        $(".close-button-img--people").on("click", function(e) {
+            $(this).parent().parent().slideUp(toggleDuration);
+            $(this).parent().parent().siblings(".box-bottom").children().children().removeClass("rotated");
+        });
+    }
 });
