9815a912be0d55471f431088d1a026eb26b1a17d
Gyeongmin Lee <metrostile@gmail.com>
2018-11-18 21:04:10 -0800

    arrow

--- js/task-list.js
+++ js/task-list.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
     $(".content--inprogress-people").on("click", function(e) {
         $(this).parent().nextAll().eq(1).hide();
         $(this).parent().nextAll().eq(0).toggle();
+        $(this).children().toggleClass("rotated");
+        $(this).next().children().removeClass("rotated");
     });
 
     $(".content--done-people").on("click", function(e) {
         $(this).parent().nextAll().eq(0).hide();
         $(this).parent().nextAll().eq(1).toggle();
+        $(this).children().toggleClass("rotated");
+        $(this).prev().children().removeClass("rotated");
     });
 
     $(".close-button-img--people").on("click", function(e) {
         $(this).parent().parent().hide();
+        $(this).parent().parent().siblings(".box-bottom").children().children().removeClass("rotated");
     });
 });
