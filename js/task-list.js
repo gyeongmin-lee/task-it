a186d1298e04fdfb94d715fa68ac9d51985863e0
Gyeongmin Lee <metrostile@gmail.com>
2018-11-18 18:04:10 -0800

    toggle people list implemented

--- js/task-list.js
+++ js/task-list.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $(".content--inprogress-people").on("click", function(e) {
+        $(this).parent().nextAll().eq(1).hide();
+        $(this).parent().nextAll().eq(0).toggle();
+    });
+
+    $(".content--done-people").on("click", function(e) {
+        $(this).parent().nextAll().eq(0).hide();
+        $(this).parent().nextAll().eq(1).toggle();
+    });
+
+    $(".close-button-img--people").on("click", function(e) {
+        $(this).parent().parent().hide();
+    });
 });
