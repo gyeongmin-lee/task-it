851fcdaa507e92e9fdea4e9d9f1823fc3b55242b
Gyeongmin Lee <metrostile@gmail.com>
2018-11-20 17:07:25 -0800

    task list menu partially implemented

--- js/task-list.js
+++ js/task-list.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     appendTasks();
     toggleList();   
-    toggleTaskState(); 
+    toggleTaskState();
+    addTaskMenu();
+    closeTaskMenu();
 
     function appendTasks() {
 
@@ -37,7 +39,36 @@ $(document).ready(function() {
         })
     }
 
-    function addTask() {
-        
+    function addTaskMenu() {
+        $(".button--add-task").on("click", function(e) {
+            var container = $(".container--task");
+            if ($("#edit-add-task").length == 0) {
+                var header = $('<div id="edit-add-task" class="box-container box-container--task">');
+                var form = $('<form></form>');
+                var inputTitle = $('<input required="required" type="text" name="task-title" placeholder="Enter a task..." class="textbox textbox--task-title" />');
+                var inputDetail = $('<textarea class="textbox textbox--area" placeholder="Enter task details..." rows="4"></textarea>');
+                var dueLabel = $('<h2 class="label label--date">Select Due Date:</h2>');
+                var inputDue = $('<input class="input--date" type="date">');
+                var boxBottom = $('<div class="box-bottom">');
+                var buttonContainer = $('<div class="inline-buttons-container">');
+                var cancelButton = $('<button class="button button--cancel-task">CANCEL</button>');
+                var saveButton = $('<button type="submit" value="Submit" class="button button--save-task">SAVE</button>');
+                boxBottom.append(buttonContainer);
+                buttonContainer.append(cancelButton, saveButton);
+                form.append(inputTitle, inputDetail, dueLabel, inputDue, boxBottom);
+                header.append(form);
+                container.append(header);
+            }   
+        })
     }
+
+    /*
+    function closeTaskMenu() {
+        $(".button--cancel-task").on("click", function(e) {
+            e.preventDefault();
+            console.log("HI");
+            $("#edit-add-task").remove();
+        })
+    }*/
 });
+
