0df3968732a3bb7fd03bfea6be198c1f1d1aa1cf
Gyeongmin Lee <metrostile@gmail.com>
2018-11-25 00:04:46 -0800

    moved everything to public

--- public/js/universal.js
+++ public/js/universal.js
@@ -1,11 +1,31 @@
-$(document).ready(function() {
+function signIn() {
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+
+}
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        window.location = 'main.html';
+    }
+});
+
+
+
+// DOM ELEMENTS
+var signInButtonElement = document.querySelector('.button-signin');
+
+signInButtonElement.addEventListener('click', signIn);
+
+$(document).ready(function () {
     $(this).scrollTop(0);
 
     goBack();
 
     function goBack() {
-        $(".close-goback").on("click", function(e) {
+        $(".close-goback").on("click", function (e) {
             window.history.back();
         })
     }
- });
+});
