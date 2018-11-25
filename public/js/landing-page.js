a4b6f27c417094f0851f202c3fa27240841fa7ce
Gyeongmin Lee <metrostile@gmail.com>
2018-11-25 00:36:04 -0800

    added firebase to all pages unified menu button design for all pages

---
+++ public/js/landing-page.js
@@ -0,0 +1,19 @@
+function signIn() {
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+
+}
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        window.location = 'main.html';
+    }
+});
+
+
+
+// DOM ELEMENTS
+var signInButtonElement = document.querySelector('.button-signin');
+
+signInButtonElement.addEventListener('click', signIn);
