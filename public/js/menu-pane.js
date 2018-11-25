651c89d8e7850cda5780135f70fd396797629990
Gyeongmin Lee <metrostile@gmail.com>
2018-11-24 23:25:42 -0800

    firebase setup added to landign page

---
+++ public/js/menu-pane.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    var menuPanel = $(".menu-panel");
+    var menuButton = $(".button--menu");
+    var exitButton = $(".close-button-img--menu");
+    var animationSpeed = 100;
+    var swipeOffset = 50;
+    var start = null;
+
+    menuButton.click(toggleMenu);
+
+    exitButton.click(toggleMenu);
+
+    menuPanel.on('touchstart', function (e) {
+        if (e.touches.length === 1) {
+            start = event.touches.item(0).clientX;
+        } else {
+            start = null;
+        }
+    });
+
+    menuPanel.on('touchend', function (e) {
+        if (start) {
+            var end = e.changedTouches.item(0).clientX;
+            if ((start - end) > swipeOffset) {
+                toggleMenu();
+            }
+        }
+    });
+
+    function toggleMenu(event) {
+        menuPanel.animate({ width: 'toggle' }, animationSpeed);
+    }
+});
---
+++ public/js/universal.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(this).scrollTop(0);
+
+    goBack();
+
+    function goBack() {
+        $(".close-goback").on("click", function(e) {
+            window.history.back();
+        })
+    }
+ });
